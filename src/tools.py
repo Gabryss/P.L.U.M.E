741d4d32e230702695b0c1246fc139a818fb9008
Gabryss <zeratul312@gmail.com>
2023-02-16 14:44:31 +0100

    Add class Tools with substract_list and opposite method

--- src/tools.py
+++ src/tools.py
@@ -1,4 +1,6 @@
 from enum import Enum
+
+
 class Color(Enum):
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -9,3 +11,24 @@ class Color(Enum):
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+
+
+class Tools():
+
+    def substract_list(self, list1, list2):
+        """
+        Substract two list and return the resulting list
+        """
+        result = []
+        for i,j in zip(list1, list2):
+            result.append(i-j)
+        return result
+    
+    def oposite(self, list_p):
+        """
+        Return the oposite of every number on a list
+        """
+        for i in range(len(list_p)):
+            list_p[i]=-list_p[i]
+        return list_p
