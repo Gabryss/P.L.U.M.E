12df7218b834cbac633103b513f5387e783f2446
Gabryss <zeratul312@gmail.com>
2023-02-16 14:43:54 +0100

    Add output colors to the terminal

--- src/generation.py
+++ src/generation.py
@@ -1,10 +1,12 @@
 from graph import Graph
 from display import Display
 from algorithm import Algorithm
+from tools import Color
 import sys
 
 GRID_SIZE = 3
 DEFAULT_PATH = "data/images/graph_0"
+
 
 def generation_logic(graph):
     """
@@ -26,20 +28,20 @@ def generation_logic(graph):
 def main(argv):
     # Check parameters  
     if len(argv) == 1 or len(argv) > 2:
-        print("Wrong number of arguments provided.\nDefault grid size selected\n\n")
+        print(f"{Color.WARNING.value}Wrong number of arguments provided.\nDefault grid size selected{Color.ENDC.value}\n\n")
         grid_size = GRID_SIZE
 
     else:
         try:
             argv[1] = int(argv[1])
         except:
-            return print("Please provide an integer")
+            return print(f"{Color.WARNING.value}Please provide an integer{Color.ENDC.value}")
 
         grid_size = argv[1]
 
 
     # Graph generation
-    print("Generation begins")
+    print(f"{Color.OKBLUE.value}Generation begins{Color.ENDC.value}\n")
     graph = Graph(grid_size)
 
     # Create the graph with the implemented logic
@@ -48,7 +50,7 @@ def main(argv):
 
 
     # Display graph
-    print("End of generation.. Exporting the graph")
+    print(f"\n{Color.OKBLUE.value}End of generation.. Exporting the graph{Color.ENDC.value}")
     display = Display()
     display.process_graph(graph)
     display.create_figure()
