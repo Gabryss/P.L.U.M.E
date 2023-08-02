7db45ecba634c24fa3e834226d09603a276e9d50
Gabryss <zeratul312@gmail.com>
2023-08-02 21:38:36 +0200

    Remove unused code related to the grid patern generation, add first draft of the Gausssian-Perlin algorithm

--- src/algorithm.py
+++ src/algorithm.py
@@ -2,124 +2,71 @@ import random as rd
 from tools import Tools
 import numpy as np
 
-
 class Algorithm():
     
-    def __init__(self, min_nodes_p = 4, loop_closure_probability_p = 10):
+    def __init__(self, graph_p, min_nodes_p = 4, loop_closure_probability_p = 10):
+        self.graph = graph_p
         self.iterations = 0
-        self.stop_algorithm = False
         self.min_nodes = min_nodes_p
         self.loop_closure_probability = loop_closure_probability_p
+        self.angles = np.arange(360)
 
-
-    def probabilistic(self, graph_p, nb_iterations):
+    def algorithm(self, selected_algorithm="gaussian_perlin"):
         """
-        With a given number of iteration/or once all nodes are dead, create a graph based on the invert of Manhattan distance.
+        Main algorithm generation
+        """
+        if selected_algorithm=="gaussian_perlin":
+            first_node_probability = self.perlin_distribution_circle()
+    
+
+    def gaussian_perlin(self):
+        """
+        Execute the Gaussian-Perlin algorithm
+        """
+        for i in range(self.min_nodes):
+            probability_array = np.zeros((3,360))
+            probability_final = np.zeros((1,360))
+
+            probability_array[0] = self.gaussian_distribution_circle()
+            probability_array[1] = self.gaussian_distribution_circle()
+            probability_array[2] = self.perlin_distribution_circle()
+            for i in range(360):
+                probability_final[0][i] = (probability_array[0][i]+probability_array[1][i]+probability_array[2][i])/3
+            # Choose an angle based on the distribution
+            chosen_angle = np.random.choice(self.angles, p=probability_final)
+
+
+    def gaussian_distribution_circle(self, direction_p):
+        """
+        Return a Gaussian distribution array based on a given direction (degree).
+        The distribution is returned in a list of 360 elements (1 element per degree).
         """
 
-        while self.iterations < nb_iterations and not self.stop_algorithm:
-            print("\nIteration number: ", self.iterations)
-
-            for node in list(graph_p.nodes):
-                # print(graph_p.nodes)
-                node = graph_p.nodes[node]
-
-                # Check if the node is active
-                if not node.active:
-                    continue
-                
-                # Check if new nodes are possibles among neighbours
-                probabilities=[]
-                possibilities = graph_p.get_possible_new_nodes(node.id)
-
-                # Check if possible new nodes
-                if len(possibilities) == 0:
-                    continue
-                    
-                else:
-                    print("node id :", node.id, "possibilities :", possibilities)
-                    # Calculate the probability according to the distance to the original node
-                    for possibility in possibilities:
-                        probabilities.append(1 / len(possibilities))
-                    
-                    # Apply probability to our list of created nodes
-                    nb_created_nodes_choice = rd.choice(range(len(possibilities)))
-                    
-                    index_list = np.random.choice(range(len(possibilities)), nb_created_nodes_choice, replace=False, p=probabilities)
-                    coordinates_choice = []
-                    for index in index_list:
-                        coordinates_choice.append(possibilities[index])
-
-                    # Create new nodes
-                    for i in range(nb_created_nodes_choice):
-                        print(graph_p.grid)
-                        coord = coordinates_choice.pop()
-                        child_graph_coordinates = self.calculate_right_graph_coordinates(node, coord)
-                        
-                        graph_p.add_node(graph_p.num_nodes+1, [node.id], None, coordinates_p=child_graph_coordinates, active_p=True, grid_coordinates_p=coord)
-                        graph_p.add_edge(node.id, graph_p.nodes[graph_p.num_nodes].id)
-                        print("Distance from parent",node.id, " to child ", graph_p.num_nodes, " is ", self.node_manhattan_distance(node,graph_p.nodes[graph_p.num_nodes]))
-
-                    if len(graph_p.nodes) >= self.min_nodes :
-                        graph_p.nodes[node.id].active = False
-                        graph_p.nb_deactivated_nodes += 1
-
-                    # Check if all nodes are dead
-                    elif graph_p.nb_deactivated_nodes == len(graph_p.nodes):
-                        self.stop_algorithm = True
-
-            self.iterations += 1
-
-        self.loop_closure(graph_p)
+        pass
 
 
-    def loop_closure(self, graph_p):
+    def perlin_distribution_circle(self, direction_p):
+        """
+        Return a Perlin distribution array based on a given direction (degree).
+        The distribution is returned in a list of 360 elements (1 element per degree).
+        """
+        pass
+
+
+    def loop_closure(self):
         """
         Post processing loop closure creation
         """
-        print("Post processing...")
-        directions = ["Left", "Right", "Up", "Down"]
-        graph = graph_p
-
-        for node_id in graph.nodes:
-            node = graph.nodes[node_id]
-
-            # Check for edges
-            edge_node = graph.check_edge(node)
-            possible_directions = directions.copy()
-
-            if edge_node:
-                for direction in directions:
-                    wall = graph.check_edge(node, direction)
-                    if wall:
-                        possible_directions.remove(direction)
-            
-            # Check neighbors on the remaining coordinates
-            neighbors_coordinates = graph.check_neighboors_safe(node, possible_directions)
-
-            neighbors = graph.get_node_coordinates_based(neighbors_coordinates)
-
-            if neighbors == None:
-                continue
-
-            else:
-                for neighbor in neighbors:
-                    if node.id in neighbor.children or node.id in neighbor.parents:
-                        continue
-                    elif neighbor.id in node.children or neighbor.id in node.parents:
-                        continue
-                    
-                    else:
-                        loop_closure_probability = rd.randint(0,self.loop_closure_probability)
-                        if loop_closure_probability == 1:
-                            node.children.append(neighbor.id)
-
-
-    def voronoi(self, graph_p):
         pass
 
-    def lattice_fibo(self, graph_p):
+
+    def voronoi(self):
         pass
+
+
+    def lattice_fibo(self):
+        pass
+
 
     def node_manhattan_distance(self, node1, node2):
         """
@@ -128,7 +75,7 @@ class Algorithm():
         """
         manhattan_distance = abs(node1.coordinates['x'] - node2.coordinates['x']) + abs(node1.coordinates['y'] - node2.coordinates['y'])
         return manhattan_distance
-    
+
 
     def manhattan_distance(self, coord1, coord2):
         """
@@ -137,25 +84,5 @@ class Algorithm():
         """
         manhattan_distance = abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
         return manhattan_distance
-    
-
-    # Tools
-
-    def calculate_right_graph_coordinates(self, parent_node_p, grid_coordinates_p):
-        """
-        Based on the parent coordinates and the new child coordinates, return the coordinates of the child on the graph
-        """
-        # Get parent coordinate
-        parent_grid_coordinates = parent_node_p.grid_coordinates
-        parent_graph_coordinates = parent_node_p.get_list_coordinates()
-
-        # Get the transform between the parent and the child
-        difference_grid_coordinates = Tools().substract_list(grid_coordinates_p, parent_grid_coordinates)
-        difference_grid_coordinates[0] = -difference_grid_coordinates[0]
-        difference_grid_coordinates.reverse()
-
-        # Get the child graph coordinates
-        child_graph_coordinates = [parent_graph_coordinates[0]+difference_grid_coordinates[0], parent_graph_coordinates[1]+difference_grid_coordinates[1]]
-        return child_graph_coordinates
 
     
