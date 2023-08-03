e4c006a3116fd7b18c78ea515c254ffc2a513b88
Gabryss <zeratul312@gmail.com>
2023-08-03 17:56:32 +0200

    Add radius around the node, parent management

--- src/node.py
+++ src/node.py
@@ -1,27 +1,25 @@
 import json
 
 class Node:
-    def __init__(self, node_id_p, parents_p=None, edges_p=None, coordinates_p=[0.0,0.0], active_p=False):
+    def __init__(self, node_id_p, parent_p=None, edges_p=None, coordinates_p=[0.0,0.0], radius_p=None, active_p=False):
         self.id = node_id_p
-        self.parents = parents_p if parents_p is not None else None
-        self.edges = edges_p if edges_p is not None else [self.parents]
+        self.parent = parent_p if parent_p is not None else None
+        self.edges = edges_p if edges_p is not None else [self.parent]
         self.coordinates = {
             'x': coordinates_p[0] if coordinates_p is not None else 0.0,
             'y': coordinates_p[1] if coordinates_p is not None else 0.0
         }
+        self.radius = radius_p if radius_p is not None else 1.0
         self.active = active_p if active_p is not None else False
 
-    def add_parent(self, parent_id_p):
-        self.parents.append(parent_id_p)
-    
     def set_parent(self, parent_p):
-        self.parents = parent_p
+        self.parent = parent_p
     
-    def get_parents(self):
-        return self.parents
+    def get_parent(self):
+        return self.parent
     
-    def has_parents(self):
-        if self.parents:
+    def has_parent(self):
+        if self.parent:
             return True
         else:
             return False
@@ -43,6 +41,12 @@ class Node:
     
     def get_coordinates(self):
         return self.coordinates
+
+    def get_radius(self):
+        return self.radius
+    
+    def set_radius(self, radius_p):
+        self.radius = radius_p
     
     def activate(self):
         self.active = True
