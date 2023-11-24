55bc2261cc0e18af6985ebd6f8b86c9ffa74d016
Gabryss <zeratul312@gmail.com>
2023-11-24 18:34:26 +0100

    Add third dimension coordinates

--- src/node.py
+++ src/node.py
@@ -1,13 +1,14 @@
 import json
 
 class Node:
-    def __init__(self, node_id_p, parent_p=None, edges_p=None, coordinates_p=[0.0,0.0], radius_p=None, active_p=False):
+    def __init__(self, node_id_p, parent_p=None, edges_p=None, coordinates_p=[0.0,0.0,0.0], radius_p=None, active_p=False):
         self.id = node_id_p
         self.parent = parent_p if parent_p is not None else None
         self.edges = edges_p if edges_p is not None else [self.parent]
         self.coordinates = {
             'x': coordinates_p[0] if coordinates_p is not None else 0.0,
-            'y': coordinates_p[1] if coordinates_p is not None else 0.0
+            'y': coordinates_p[1] if coordinates_p is not None else 0.0,
+            'z': coordinates_p[2] if coordinates_p is not None else 0.0,
         }
         self.radius = radius_p if radius_p is not None else 1.0
         self.active = active_p if active_p is not None else False
@@ -41,7 +42,7 @@ class Node:
         self.coordinates = coordinates_p
 
     def get_list_coordinates(self):
-        return [self.coordinates['x'], self.coordinates['y']]
+        return [self.coordinates['x'], self.coordinates['y'], self.coordinates['z']]
     
     def get_coordinates(self):
         return self.coordinates
