ef846ab8ab0148a9cc26bb2883f74350243642ec
Gabryss <zeratul312@gmail.com>
2024-01-17 19:21:17 +0100

    Add point interpolation, needs to change the interpolation process to cleaner one

--- src/display.py
+++ src/display.py
@@ -25,7 +25,7 @@ class Display():
         self.nodes_size = node_width_p
         self.edges_width = edge_width_p
         self.save_image_path = Config.PLUME_DIR.value+"/data/"+self.generation_name+self.nb_graphs+"/graph"+Config.IMAGE_FORMAT.value
-    
+        self.save_html_image_path = Config.PLUME_DIR.value+"/data/"+self.generation_name+self.nb_graphs+"/graph.html"
 
     def save_image(self):
         """
@@ -43,6 +43,9 @@ class Display():
     
         self.figure.write_image(self.save_image_path, engine="kaleido")
 
+        if self._3dimension:
+            self.figure.write_html(self.save_html_image_path)
+
 
     def process_graph(self, graph_p):
         """
@@ -57,40 +60,9 @@ class Display():
         """
         Render the graph in the figure object
         """
-        import matplotlib.pyplot as plt
         if self._3dimension :
-            # x = np.array(range(-20, 20, 0.1))
-            # y = np.array(range(-20, 20, 0.1))
-            # z = np.array(range(-20, 20, 0.1))
-            # Xgrid, Ygrid, Zgrid = np.meshgrid(x,y,z)
+   
             Xgrid, Ygrid, Zgrid = np.mgrid[-10:10:50j, -10:10:50j, -10:10:50j]
-
-            # fig = plt.figure()
-            # ax = fig.add_subplot(projection='3d')
-            # for node in self.nodes:
-            #     cX, cY, cZ = self.nodes[node].coordinates['x'],self.nodes[node].coordinates['y'],self.nodes[node].coordinates['z']
-            #     radius = self.nodes[node].radius
-                
-            #     check = (Xgrid - cX)**2 + (Ygrid - cY)**2 + (Zgrid - cZ)**2 <= radius**2
-            #     # print(check, check.shape)
-            #     # print(Xgrid[check], Xgrid[check].shape)
-                
-            #     # array = np.r_[array,[[self.nodes[node].coordinates['x'],self.nodes[node].coordinates['y'],self.nodes[node].coordinates['z']]]]
-            #     ax.scatter(Xgrid[check], Ygrid[check], Zgrid[check], color='green')
-
-            # ax.set_title('сетка из точек 100х100',
-            #             fontfamily = 'monospace',
-            #             fontstyle = 'normal',
-            #             fontweight = 'bold',
-            #             fontsize = 10)
-            # ax.set_xlabel("Value", fontsize=14)
-            # ax.set_ylabel("Square of Value", fontsize=14)
-
-            # ax.set_xlim3d(-50, 50)
-            # ax.set_ylim3d(-50, 50)
-            # ax.set_zlim3d(-50, 50)
-
-            # plt.show()
             count = 0
             for node in self.nodes:
                 count +=1
@@ -98,55 +70,35 @@ class Display():
                     cX, cY, cZ = self.nodes[node].coordinates['x'],self.nodes[node].coordinates['y'],self.nodes[node].coordinates['z']
                     radius = 0.5
                     values = (Xgrid - cX)**2 + (Ygrid - cY)**2 + (Zgrid - cZ)**2 <= radius**2
-                    # values=values.flatten()
-
-
-
-
-                # test = plotly.graph_objects.Volume(
-                #     x=Xgrid.flatten(),
-                #     y=Ygrid.flatten(),
-                #     z=Zgrid.flatten(),
-                #     value=values.flatten(),
-                #     isomin=-0.1,
-                #     isomax=0.8,
-                #     opacity=0.1, # needs to be small to see through all surfaces
-                #     surface_count=25, # needs to be a large number for good volume rendering
-                # )
-                # data_3d.append(plotly.graph_objects.Volume(
-                #     x=Xgrid.flatten(),
-                #     y=Ygrid.flatten(),
-                #     z=Zgrid.flatten(),
-                #     value=values.flatten(),
-                #     isomin=-0.1,
-                #     isomax=0.8,
-                #     opacity=0.1, # needs to be small to see through all surfaces
-                #     surface_count=17, # needs to be a large number for good volume rendering
-                # ))
                 
                 else:
-                    cX, cY, cZ = self.nodes[node].coordinates['x'],self.nodes[node].coordinates['y'],self.nodes[node].coordinates['z']
+                    cX, cY, cZ = self.nodes[node].coordinates['x'], self.nodes[node].coordinates['y'], self.nodes[node].coordinates['z']
+                    
                     radius = 0.5
                     new_values = (Xgrid - cX)**2 + (Ygrid - cY)**2 + (Zgrid - cZ)**2 <= radius**2
-                    # new_values.flatten()
+                    if count != len(self.nodes):
+                        cX_next, cY_next, cZ_next = self.nodes[count].coordinates['x'], self.nodes[count].coordinates['y'], self.nodes[count].coordinates['z']
+                        
+                        c = np.array([cX, cY, cZ])
+                        c_next = np.array([cX_next, cY_next, cZ_next])
+                        dist = np.linalg.norm(c - c_next)
+
+                        if dist < 1.0:
+                            x_f = np.linspace(c[0],c_next[0],int(dist))
+                            y_f = np.linspace(c[1],c_next[1],int(dist))
+                            z_f = np.linspace(c[2],c_next[2],int(dist))
+                        
+                        else:
+                            x_f = np.linspace(c[0],c_next[0],int(dist*2))
+                            y_f = np.linspace(c[1],c_next[1],int(dist*2))
+                            z_f = np.linspace(c[2],c_next[2],int(dist*2))
+                        
+                        for i in range(len(x_f)):
+                            new_values+= (Xgrid - x_f[i])**2 + (Ygrid - y_f[i])**2 + (Zgrid - z_f[i])**2 <= radius**2
 
                     values = np.logical_or(values, new_values)
-                    # for i in range(len(values)):
-                    #     for j in range(len(values)):
-                    #         for k in range(len(values)):
-                    #             if new_values[i][j][k] == True:
-                    #                 values[i][j][k] == True
-                    
-                    # print(values, values.shape)
-                    # print(Xgrid[values], Xgrid[values].shape)
-                
-            
-            self.figure.add_trace(go.Streamtube(x=[0, 0, 1], y=[0, 1, 2], z=[0, 0, 0], 
-                                       u=[0, 0, 0], v=[1, 1, 1], w=[0, 0, 0]))
 
-            # X, Y, Z = np.mgrid[-1:1:30j, -1:1:30j, -1:1:30j]            # values = np.sin(X*Y*Z) / (X*Y*Z)
-            # values = np.sin(np.pi*X) * np.cos(np.pi*Z) * np.sin(np.pi*Y)
-            # self.figure = go.Figure(data=data_3d)
+
             self.figure = go.Figure(data=go.Volume(
                 x=Xgrid.flatten(),
                 y=Ygrid.flatten(),
@@ -154,76 +106,17 @@ class Display():
                 value=values.flatten(),
                 isomin=-1,
                 isomax=1,
-                opacity=0.1, # needs to be small to see through all surfaces
+                opacity=0.2, # needs to be small to see through all surfaces
                 surface_count=20, # needs to be a large number for good volume rendering
+                caps= dict(x_show=True, y_show=True, z_show=True, x_fill=1),
                 ))
             
-            df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/streamtube-wind.csv').drop(['Unnamed: 0'],axis=1)
-            print(df)
-            print(df['x'])
-            x, y, z = np.mgrid[0:10, 0:10, 0:10]
-            x = x.flatten()
-            y = y.flatten()
-            z = z.flatten()
-
-            u = np.zeros_like(x)
-            v = np.zeros_like(y)
-            w = z**2
-            self.figure.add_trace(go.Streamtube(x=x,
-                                                y=y,
-                                                z=z,
-                                                u=u,
-                                                v=v,
-                                                w=w))
-
-            # self.figure = go.Figure(data=go.Isosurface(
-            #     x=[0,0,0,0,2,2,2,2],
-            #     y=[3,0,3,0,3,0,3,0],
-            #     z=[2,2,5,5,2,2,5,5],
-            #     value=[2,2,3,4,5,6,7,8],
-            #     isomin=2,
-            #     isomax=6,
-            #     colorscale='magma',
-            # ))
-
+            self.figure.update_layout(scene_xaxis_showticklabels=False,
+                  scene_yaxis_showticklabels=False,
+                  scene_zaxis_showticklabels=False)
+            
             
             self.figure.update_layout(template='plotly_dark', title=f"Generation {self.generation_name}")
-
-
-
-
-            # self.figure.update_layout(scene_xaxis_showticklabels=False,
-            #                 scene_yaxis_showticklabels=False,
-            #                 scene_zaxis_showticklabels=False)
-
-            # for edge in self.edges:
-            #     # df = px.data.gapminder().query("continent=='Europe'")
-            #     data = {'x':self.nodes[edge[0]].coordinates['x'], 'y':self.nodes[edge[0]].coordinates['y'],  'z':self.nodes[edge[0]].coordinates['z']}
-            #     df1 = pd.DataFrame([data])
-            #     df = pd.concat([df, df1], ignore_index=True)
-
-            #     data = {'x':self.nodes[edge[1]].coordinates['x'], 'y':self.nodes[edge[1]].coordinates['y'],  'z':self.nodes[edge[1]].coordinates['z']}
-            #     df1 = pd.DataFrame([data])
-            #     df = pd.concat([df, df1], ignore_index=True)
-            # print(df)
-
-
-                
-            # self.figure = px.line_3d(df, x="x", y="y", z="z")
-            # self.figure.show()
-
-            # df = px.data.iris()
-            
-            
-            #Scatter
-            # self.figure = px.scatter_3d(df, x='x', y='y', z='z',
-            #             size_max=18,
-            #             opacity=0.7)
-            
-
-            # # tight layout
-            # self.figure.update_layout(margin=dict(l=0, r=0, b=0, t=0))
-            self.figure.show()
 
         else:
             # Create a scatter plot for each node
@@ -249,5 +142,4 @@ class Display():
             
             self.figure.update_traces(textposition='top center')
             self.figure.update_layout(template='plotly_dark', title=f"Generation {self.generation_name}")
-            self.figure.show()
 
