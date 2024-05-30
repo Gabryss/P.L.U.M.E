61e216eaaf055c9240e31a020dfe60b254df5973
Gabryss <zeratul312@gmail.com>
2024-05-30 12:34:21 +0200

    Fix typo

--- src/config.py
+++ src/config.py
@@ -14,7 +14,7 @@ class Config(Enum):
     # ==== General settings ====
     # ==========================
     NB_GENERATION = 1
-    GENERATION_SIZE = (50,10,10)            # Size of the generated environment
+    GENERATION_SIZE = (500,100,100)            # Size of the generated environment
     
     #Graph image
     IMAGE_FORMAT = ".png"
@@ -27,15 +27,15 @@ class Config(Enum):
     GENERATE_MESH = True                    # Use blender to generate the mesh
     SAVE_MESH = False                       # Save the mesh in data folder
     BAKE_TEXTURE = False                    # Create, bake and save the textures (Color, Normal and Roughness maps)
-    TYPE_OF_UNDERGROUND = "MINE"            # Available: MINE (#Not available yet), CAVE
+    TYPE_OF_UNDERGROUND = "CAVE"            # Available: MINE (#Not available yet), CAVE
 
     # ==== Advanced settings ====
     # ===========================
-    NB_NODES = 50                           # Number of nodes in the generation
-    MAX_CREATED_NODE_ON_CIRCLE = 2          # Number of nodes created per nodes
+    NB_NODES = 500                          # Number of nodes in the generation
+    MAX_CREATED_NODE_ON_CIRCLE = 2          # Maximal number of nodes created per nodes
     MAX_RADIUS_NODE = 7.0                   # Distance between the nodes
     DEFAULT_LOOP_CLOSURE_PROBABILITY = 10   # Probability of connecting two close nodes
-    SELECTED_ALGORITHM = "mine"  # Available: gaussian_perlin, mine
+    SELECTED_ALGORITHM = "gaussian_perlin"  # Available: gaussian_perlin, mine
     TEXTURE_SIZE = 1024                     # 32768(Don't try this), 16384 (64GB RAM or more is needed), 8192 (32GB RAM or more is needed), 4096, 1024 pixels
     MAX_MESH_TRIANGLES = 1000000            # 1Million triangles: 1000000 (Upper threshold for vscode obj visualizer)
     FINAL_DECIMATION = False
